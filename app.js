3a54a2358873c1ff61d5e71fbe3054ca601446a2
Picard Clown <picardc@student.21-school.ru>
2023-05-19 00:58:03 +0300

    add front

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 require("dotenv").config();
 
 app.use(bodyParser.json());
@@ -15,6 +16,15 @@ const start = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_DB);
 		console.log("Successful DB connection");
+
+		// ... other app.use middleware
+		app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+		// ...
+		// Right before your app.listen(), add this:
+		app.get("*", (req, res) => {
+			res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+		});
 
 		app.listen(PORT, () => {
 			console.log(`App started on port ${PORT}`);
